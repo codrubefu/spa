10d2689bdea8093187edcc248da47505d10f3f4a
CodrutBefuAccess <codrut_befu@yahoo.com>
2024-11-07 19:07:15 +0200

    Update soap.php

--- soap.php
+++ soap.php
@@ -28,7 +28,7 @@ class soap {
 		// Execute the request
 		$response  = curl_exec( $ch );
 		$http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
-
+print_r($response);
 		// Check for errors
 		if ( $response === false || $http_code !== 200 ) {
 			error_log( 'Eroare cURL: ' . curl_error( $ch ) . ' (Cod HTTP: ' . $http_code . ')' );
