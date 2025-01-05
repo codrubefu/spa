db3e945fb3b23ecf63b58b2a90ddc57286c663c7
CodrutBefuAccess <codrut_befu@yahoo.com>
2025-01-05 17:27:44 +0200

    remove cnp

--- order/order.php
+++ order/order.php
@@ -64,7 +64,7 @@ class order {
 		$soapCartInfo['BMI'] = implode('#',$beneficiariesIds); // Beneficiary MasterSPA ID, Numeric(9): MasterSPA Client ID for Beneficiary, equal to MID if none
 		$soapCartInfo['CRD'] = implode( '#', $items['crd'] ); // CM12 cifre   Voucher/Card Code (Buyer), VarChar(20): Barcode/code from Voucher/Gift Card/Member Card for Buyer
 		$soapCartInfo['BCD'] = implode( '#', $items['bcd'] ); // Voucher/Card Code (Family 2), VarChar(20): Voucher/Gift Card for Beneficiary, empty if none
-		$soapCartInfo['CNP'] = '1831028336374'; // Buyer Personal ID, VarChar(13): Buyer's personal ID number
+		$soapCartInfo['CNP'] = '0000000000000'; // Buyer Personal ID, VarChar(13): Buyer's personal ID number
 		$soapCartInfo['REN'] = implode( '#', $items['ren'] );; // Renewal Flag, Char(1): "0"=not renewal, "1"=is renewal
 		$soapCartInfo['TYP'] = implode( '#', $items['type'] );; // List of Type of Sale, VarChar(30): Types of sales separated by '#'
 		$soapCartInfo['PTN'] = $partnerId; // Partner ID, Numeric(9): Company ID for issuing the fiscal invoice
